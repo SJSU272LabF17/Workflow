f3b1694fe65e66f224e23216521fe0c9844b2be2
yulu9206@gmail.com <yulu9206@Lus-MacBook-Air.local>
2017-11-10 12:42:33 -0800

    combining server with client

---
+++ apps/first_app/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+  url(r'^main$', views.index),
+  url(r'^register$', views.register),
+  url(r'^login$', views.login),
+  url(r'^logout$', views.logout),
+  url(r'^dashboard$', views.dashboard),
+  url(r'^wish_items/create$', views.create_new),
+  url(r'^create_process$', views.create_process),
+  url(r'^wish_items/(?P<item_id>\d+)$', views.item),
+  url(r'^add_wish/(?P<item_id>\d+)$', views.add_wish),
+  url(r'^remove_wish/(?P<item_id>\d+)$', views.remove_wish),
+  url(r'^delete_item/(?P<item_id>\d+)$', views.delete_item),
+]
---
+++ apps/first_app/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User, Item
+
+def index(request):
+    context = {
+    'users' : User.objects.all()
+    }
+    return render(request, 'first_app/index.html', context)
+
+def register(request):
+    if request.POST['pw'] != request.POST['c_pw']:
+        messages.error(request, "Passwords does not match")
+        return redirect('/main') 
+    postData = {
+    'name': request.POST['name'],
+    'username': request.POST['username'],
+    'h_date': request.POST['h_date'],
+    'pw': request.POST['pw'],
+    }
+    result = User.objects.register(postData)
+    if result[0] == False:
+        for error in result[1]:
+            messages.error(request, error)
+        return redirect('/main')
+    else:
+        request.session['user_id'] = result[1].id
+        return redirect('/dashboard') 
+
+def login(request):
+    postData = {
+    'username': request.POST['username'],
+    'pw': request.POST['pw'],
+    }
+    result = User.objects.login(postData)
+    if result[0] == False:
+        for error in result[1]:
+            messages.error(request, error)
+        return redirect('/main')
+    else:
+        request.session['user_id'] = result[1].id
+        return redirect('/dashboard')
+
+def logout(request):
+    request.session['user_id'] = None
+    return redirect('/main')
+
+def dashboard(request):
+    this_user = User.objects.filter(id=request.session['user_id']).first()
+    context = {
+    'user': this_user,
+    'items': Item.objects.all()
+    }
+    return render(request, 'first_app/dashboard.html', context)
+
+def create_new(request):
+    return render(request, 'first_app/create_item.html')
+
+def create_process(request):
+    name = request.POST.get('name', False)
+    # print name
+    postData = {
+    'name': name,
+    }
+    result = Item.objects.validate(postData, request.session['user_id'])
+    if result[0] == False:
+        for error in result[1]:
+            messages.error(request, error)
+        return redirect('/wish_items/create')
+    else:
+        return redirect('/dashboard') 
+
+def item(request, item_id):
+    item = Item.objects.filter(id=item_id).first()
+    context = {
+    'item': item
+    }
+    return render(request, 'first_app/item.html', context)
+
+def add_wish(request, item_id):
+    this_user = User.objects.filter(id=request.session['user_id']).first()
+    this_item = Item.objects.filter(id=item_id).first()
+    this_item.wished_users.add(this_user)
+    return redirect('/dashboard')
+
+def remove_wish(request, item_id):
+    this_user = User.objects.filter(id=request.session['user_id']).first()
+    this_item = Item.objects.filter(id=item_id).first()
+    this_item.wished_users.remove(this_user)
+    return redirect('/dashboard')
+
+def delete_item(request, item_id):
+    Item.objects.filter(id=item_id).first().delete()
+    return redirect('/dashboard')
